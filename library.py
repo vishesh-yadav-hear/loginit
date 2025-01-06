913c49e5b4591bcc2131b97d57b4bbf50b29c428
vishesh-yadav-hear <visheshyadavhear@gmail.com>
2025-01-06 04:35:52 +0000

    SOME STARTING MODIFICATION OF LIBRERY

--- library.py
+++ library.py
@@ -1,7 +1,29 @@
-# my_library.py
+import pandas as pd
+import os
+user_name = []
+user_password = []
 
-def tr(name):
-    """
-    Function to greet the user with their name.
-    """
-    return f"Hello, {name}!"
+# Function to create a new user
+def create_user(name, password):
+    user_name.append(name)
+    user_password.append(password)
+    user_diction = {"Name": user_name, "Password": user_password}
+    user_df = pd.DataFrame(user_diction)
+    if os.path.exists("users.csv"):
+        user_df.to_csv("users.csv", index=False, mode="a", header=False)
+    else:
+      user_df.to_csv("users.csv", index=False)
+    return "Account created successfully"
+
+# Function to log in
+def login(name, password):
+    user_df = pd.read_csv("users.csv")
+    if name in user_df['Name'].to_list():
+        user_index = user_df[user_df['Name'] == name].index[0]
+        if password == user_df.loc[user_index, 'Password']:
+            return "Login successful"
+        else:
+            return "Incorrect password"
+    else:
+        return "User not found"
+
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-import library as lb
-print(lb.tr("ram"))
+import library as lib
+from flask import Flask,render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+@app.route('/register', methods = ['POST'])
+def register():
+  if request.method == 'POST':
+    user_name = request.form['user_id']
+    user_password = request.form['password']
+  lib.create_user(user_name, user_password)
+  message = lib.login(user_name, user_password)
+  return render_template("index.html", message=message)
+@app.route('/login_visite', methods = ['GET'])
+def login_visite():
+  return render_template("log-in.html")
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+  if request.method == 'POST':
+    user_name = request.form['user_id']
+    user_password = request.form['password']
+    lib.login(user_name, user_password)
+    message = lib.login(user_name, user_password)
+  return render_template("index.html", message=message)
+ 
+
+if __name__ == '__main__':
+  app.run(debug=True)
